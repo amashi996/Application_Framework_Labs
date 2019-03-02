7428e7130552348314e83b7dad3a08e79087f104
manoj997 <dharmakumar338@gmail.com>
2019-03-02 13:02:56 +0530

    lab03_v3

--- Lab_03/AppContainer.jsx
+++ Lab_03/AppContainer.jsx
@@ -2,6 +2,7 @@
 
 import React, {Component} from 'react';
 
+import AddUser from './AddUser';
 import Users from './Users';
 
 
@@ -18,9 +19,19 @@ export default class AppContainer extends Component{
         }
     }
 
+    addUser(user){
+        this.setState(state =>({
+            users: state.users.concat({
+                id : Date.now(),
+                name : user.name
+            })
+        }))
+    }
+
     render() {
         return <div>
             <h2>Users App</h2>
+            <AddUser addUser={user=>this.addUser(user)}/>
             <Users users={this.state.users}/>
         </div>;
     }
